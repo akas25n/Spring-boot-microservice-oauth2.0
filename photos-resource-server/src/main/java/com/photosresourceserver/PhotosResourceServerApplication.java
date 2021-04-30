2253c00aceb48cca61e1cde9455fbbd08e051bca
Abdur Rahim <abdur.rahim@hermesworld.com>
2021-04-30 15:05:35 +0200

    implemented role based authorization in album-resource-server

--- photos-resource-server/src/main/java/com/photosresourceserver/PhotosResourceServerApplication.java
+++ photos-resource-server/src/main/java/com/photosresourceserver/PhotosResourceServerApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class PhotoResourceServerApplication {
+public class PhotosResourceServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PhotoResourceServerApplication.class, args);
+        SpringApplication.run(PhotosResourceServerApplication.class, args);
     }
 
 }
