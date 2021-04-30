3e8d694623efbafe99bdfec91d1f1c4293eee964
Abdur Rahim <abdur.rahim@hermesworld.com>
2021-04-30 15:10:42 +0200

    implemented role based authorization in album-resource-server

---
+++ albums-resource-server/src/main/java/com/albumsresourceserver/security/WebSecurity.java
@@ -0,0 +1,28 @@
+package com.albumsresourceserver.security;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        JwtAuthenticationConverter authenticationConverter = new JwtAuthenticationConverter();
+        authenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
+
+        http
+            .authorizeRequests()
+            .antMatchers(HttpMethod.GET, "/api/albums")
+            .hasRole("admin")
+            .anyRequest().authenticated()
+            .and()
+            .oauth2ResourceServer()
+            .jwt()
+            .jwtAuthenticationConverter(authenticationConverter);
+    }
+}
